3dcf6d5aaeaafb3684fe1b90ead238895db14ca8
William Boswell <doujones@Williams-MacBook-Air.local>
2021-07-10 23:51:01 -0400

    Completed LC1_TwoSum

--- arrays/LC1_TwoSum/index.js
+++ arrays/LC1_TwoSum/index.js
@@ -1,3 +1,15 @@
-function twoSum(arr, target) {}
+function twoSum(nums, target) {
+  const previousvalues = {}  
+  for(let i = 0; i < nums.length; i++){
+  const currentNumber = nums[i]
+  const neededvalue = target - currentNumber 
+  const index2 = previousvalues[neededvalue]
+  if(index2 != null){
+      return [index2, i]
+  } else {
+      previousvalues[currentNumber] = i
+  }
+  }
+}
 
 module.exports = twoSum;
